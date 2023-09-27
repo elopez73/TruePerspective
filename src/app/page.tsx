02efd49dc9d81ba2579fa49c2d1f9734928cc017
elopez73 <esaulopez14@gmail.com>
2023-09-27 07:16:44 -0700

    feat:added Lightbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,31 @@ import { Tab } from "@headlessui/react";
 import Link from "next/link";
 import Image from "next/image";
 import Masonry from "react-masonry-css";
+import type { LightGallery } from "lightgallery/lightgallery";
+import LightGalleryComponent from "lightgallery/react";
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+
 import wedding from "public/wedding.jpg";
 import wedding1 from "public/wedding1.jpg";
 import wedding2 from "public/wedding2.jpg";
 import wedding3 from "public/wedding3.jpg";
 import wedding4 from "public/wedding4.jpg";
-import wedding5 from "public/wedding4.jpg";
+import wedding5 from "public/wedding5.jpg";
+import { useRef } from "react";
 
 const tabs = [
 	{ key: "all", display: "All" },
 	{ key: "wedding", display: "Wedding" },
 	{ key: "personal", display: "Personal" },
 ];
+const images = [wedding, wedding1, wedding2, wedding3, wedding4, wedding5];
 export default function Home() {
+
+	const lightboxRef = useRef < LightGallery | null > (null);
 	return (
 		<div className=" h-full bg-white align-center overflow-auto">
 			<header className="fixed w-full top-0 z-10 flex justify-between items-center h-[10%] px-10">
@@ -62,37 +74,35 @@ export default function Home() {
 									className="flex gap-4"
 									columnClassName="my-masonry-grid_column"
 								>
-									<Image
-										src={wedding}
-										alt="wed1"
-										className="my-4"
-									/>
-									<Image
-										src={wedding1}
-										alt="wed1"
-										className="my-4"
-									/>
-									<Image
-										src={wedding2}
-										alt="wed1"
-										className="my-4"
-									/>
-									<Image
-										src={wedding3}
-										alt="wed1"
-										className="my-4"
-									/>
-									<Image
-										src={wedding4}
-										alt="wed1"
-										className="my-4"
-									/>
-									<Image
-										src={wedding5}
-										alt="wed1"
-										className="my-4"
-									/>
+									{images.map((image,index) => (
+										<Image
+											key={image.src}
+											src={image}
+											alt="wed1"
+											className="my-4 hover:opacity-70 cursor-pointer"
+											placeholder="blur"
+											onClick={() => {
+												lightboxRef.current?.openGallery(index);
+											}}
+										/>
+									))}
 								</Masonry>
+
+								<LightGalleryComponent
+									onInit={(ref) => {
+										if (ref) {
+											lightboxRef.current = ref.instance
+										}
+									}}
+									speed={500}
+									plugins={[lgThumbnail, lgZoom]}
+									dynamic
+									dynamicEl={images.map((image) => ({
+										src: image.src,
+										thumb:image.src
+
+									}))}
+								/>
 							</Tab.Panel>
 							<Tab.Panel>Wedding shoot</Tab.Panel>
 							<Tab.Panel>Personal</Tab.Panel>
